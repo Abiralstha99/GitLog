298964973bfcf3fc669228a62faf480735412a33
Abiral Shrestha <abiralstha333@gmail.com>
2025-06-20 20:53:05 -0400

    Displayed books on homepage using data from PostgreSQL

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import booksRouter from './routes/booksRouter.js';
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
