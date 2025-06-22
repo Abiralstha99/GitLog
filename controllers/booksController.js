50580f6011d8075a5799072f36048d56128f979b
Abiral Shrestha <abiralstha333@gmail.com>
2025-06-21 21:41:48 -0400

    Add form to create new books and store them in database

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,20 +1,36 @@
-import db from '../model/model.js';
+import db from "../model/model.js";
 
-async function getBooks(req,res){
-    try {
-        const result = await db.query("SELECT * from books ")
-        const books = result.rows;
-        res.render("index", { books })
-    } catch (error) {
-        res.status(500).send("Error")
-    }
+async function getBooks(req, res) {
+  try {
+    const result = await db.query("SELECT * from books ");
+    const books = result.rows;
+    res.render("index", { books });
+  } catch (error) {
+    res.status(500).send("Error");
+  }
 }
 
-async function renderNewForm(req,res) {
-    try {
-        res.render("form")
-    } catch (error) {
-        res.status(500).send("Error")
-    }
+async function renderNewForm(req, res) {
+  try {
+    res.render("form");
+  } catch (error) {
+    res.status(500).send("Error");
+  }
 }
-export { getBooks, renderNewForm};
+
+async function postBook(req, res) {
+  try {
+    const { title, author, rating, read_date, notes, cover_id } = req.body;
+    if (!title) {
+      return res.status(400).send("Book title is required");
+    }
+    await db.query(
+      "INSERT INTO books (title, author, rating, read_date, notes, cover_id) VALUES ($1, $2, $3, $4, $5, $6)",
+      [title, author, rating, read_date, notes, cover_id]
+    );
+    res.redirect('/');
+  } catch (error) {
+    res.status(500).send("Error");
+  }
+}
+export { getBooks, renderNewForm, postBook };
--- routes/booksRouter.js
+++ routes/booksRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getBooks, renderNewForm } from '../controllers/booksController.js';
+import { getBooks, renderNewForm, postBook } from '../controllers/booksController.js';
 
 const router = express.Router();
 
 router.get('/', getBooks);
 
 router.get('/new', renderNewForm);
+
+router.post('/new', postBook);
 
 export default router;
