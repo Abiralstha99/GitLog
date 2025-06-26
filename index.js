611b7b92c647298d29a94a90aead24723dd591e3
Abiral Shrestha <abiralstha333@gmail.com>
2025-06-26 10:11:51 -0400

    Added add button

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.set('view engine', 'ejs');
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(express.static("public"));
 
 // routes
 console.log({
@@ -18,6 +19,7 @@ console.log({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 });
+
 app.use("/", booksRouter);
 app.listen(PORT, () =>{
     console.log(`Server running at PORT ${PORT}`);
