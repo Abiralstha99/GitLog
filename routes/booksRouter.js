99144b66ac75a61700b1c09f72a2cefac8920cf9
Abiral Shrestha <abiralstha333@gmail.com>
2025-08-14 12:32:02 -0400

    add signup GET and POST routes to booksRouter

--- routes/booksRouter.js
+++ routes/booksRouter.js
@@ -8,18 +8,27 @@ import {
   updateBook
 } from "../controllers/booksController.js";
 
+import {getSignUp, signUp} from "../controllers/signUp.js"
+import { login } from '../controllers/authenticate.js';
+
 const router = express.Router();
 
 router.get("/", getBooks);
 
 router.get("/new", renderNewForm);
 
-router.post("/new", postBook);
+router.post("/new", authenticateJWT, postBook);
 
-router.post("/delete/:id", deleteBook);
+router.post("/delete/:id", authenticateJWT, deleteBook);
+
+router.post('/edit/:id', authenticateJWT, updateBook);
 
 router.get("/edit/:id", renderCurrentForm);
 
-router.post('/edit/:id', updateBook);
+router.get('/signup',getSignUp);
+
+router.post('/signup', signUp);
+
+router.post('/login', login);
 
 export default router;
