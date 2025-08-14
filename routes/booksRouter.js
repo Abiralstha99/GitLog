fd78e5ad839c82c55c818ade5106c27fa4ad2efe
Abiral Shrestha <abiralstha333@gmail.com>
2025-08-14 15:33:31 -0400

    protected only specific routes, added controller-based logout

--- routes/booksRouter.js
+++ routes/booksRouter.js
@@ -8,27 +8,25 @@ import {
   updateBook
 } from "../controllers/booksController.js";
 
-import {getSignUp, signUp} from "../controllers/signUp.js"
-import { login } from '../controllers/authenticate.js';
+import { getSignUp, signUp } from "../controllers/signUp.js";
+import { login, logout, authenticateJWT } from '../controllers/authenticate.js';
 
 const router = express.Router();
 
+// Public routes
 router.get("/", getBooks);
-
 router.get("/new", renderNewForm);
+router.get("/edit/:id", renderCurrentForm);
+router.get('/signup', getSignUp);
+router.post('/signup', signUp);
+router.post('/login', login);
 
+// Protected routes (JWT required)
 router.post("/new", authenticateJWT, postBook);
-
 router.post("/delete/:id", authenticateJWT, deleteBook);
-
 router.post('/edit/:id', authenticateJWT, updateBook);
 
-router.get("/edit/:id", renderCurrentForm);
-
-router.get('/signup',getSignUp);
-
-router.post('/signup', signUp);
-
-router.post('/login', login);
+// Logout route: use controller to clear JWT cookie and redirect with flash message
+router.post('/logout', logout);
 
 export default router;
